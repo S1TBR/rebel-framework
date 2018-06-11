073068f1b454c69531e951b09a5d451aa7e4a63d
Magdy Moustafa <sakafa_magdy@yahoo.com>
2018-06-11 16:35:27 -0400

    Update update.sh

--- modules/update.sh
+++ modules/update.sh
@@ -2,7 +2,9 @@
 current_path=$( cd .. ; pwd )
 cd /tmp
 wget https://raw.githubusercontent.com/rebellionil/rebel-framework/master/modules/.is_up_to_date --output-document=.is_up_to_date 2> /dev/null
-
+if [[ $(cat .is_up_to_date ) =~ '+' ]] ; then
+   resetup="true"
+fi
 if ! [[ $( diff ${current_path}/modules/.is_up_to_date .is_up_to_date ) == "" ]] ; then
     echo -e "[*] Checking for updates.. "
     echo -n "[+] New Updates are avilable, Update the Framework ? [Y/n] "
@@ -16,12 +18,18 @@ if ! [[ $( diff ${current_path}/modules/.is_up_to_date .is_up_to_date ) == "" ]]
        echo -e "[+] Cloning Rebel-framework... done"
        mv rebel.sh ${current_path}
        mv setup.sh ${current_path}
-       rm -rf ${current_path}/modules ;  mv modules ${current_path}
+       mv ${current_path}/modules/phish/Server/ngrok ${currnet_path}
+       rm -rf ${current_path}/modules
+       mv modules ${current_path}
+       mv ${current_path}/ngrok ${current_path}/modules/phish/Server/ngrok 
        cd ..
        rm -rf rebel-framework
-       cd ${current_path} ; bash setup.sh
+       if [[ $resetup == "true" ]] ; then
+          cd ${current_path} ; xterm -T "SETUP" -e "bash setup.sh"
+       fi
        echo "[+] The Framework is uptodate ."
        kill `ps aux | grep rebel.sh | grep -v color  | awk {' print $2 '}` 2> /dev/null 1> /dev/null
     fi
 fi
+
 
